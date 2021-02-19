05e534be44813ebde71c03dc615e57cb3dee3b9a
student <dorian.pettway-bowman@ngc.com>
2021-02-19 03:30:13 +0000

    Add logic and test

--- src/main/java/com/simplilearn/ReviewAnalyzer/ReviewAnalyzerApplication.java
+++ src/main/java/com/simplilearn/ReviewAnalyzer/ReviewAnalyzerApplication.java
@@ -7,11 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReviewAnalyzerApplication {
     public static double getWordCount(String review){
         	int count = 0;
-
-            	String string[] = review.toLowerCase().split("([,.\\s]+)");
-            	for(String s : string){
+      	 
+            	String string[] = review.toLowerCase().split("([,.\\s]+)");  
+            	for(String s : string){  
    	 	count++;
-            	}
+            	}  
    	 return count;
     }
     public static void main(String[] args) {
--- src/test/java/com/simplilearn/ReviewAnalyzer/ReviewAnalyzerApplicationTests.java
+++ src/test/java/com/simplilearn/ReviewAnalyzer/ReviewAnalyzerApplicationTests.java
@@ -1,16 +1,15 @@
 package com.simplilearn.ReviewAnalyzer;
 
-
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ReviewAnalyserApplicationTests {
+class ReviewAnalyzerApplicationTests {
 
     private ReviewAnalyzerApplication analyzer = new ReviewAnalyzerApplication();
-    @Test
-    public void testWordCount() {
-   	 assertEquals(7,analyzer.getWordCount("Train to win in the digital economy"));
+    @Test	 
+    public void testWordCount() {     	 
+   	 assertEquals(7,analyzer.getWordCount("Train to win in the digital economy"));	 
     }
 }
